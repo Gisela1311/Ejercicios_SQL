d2ab3792bc8c0696cb1dec95cce3c9b88b3d186f
Gisela Palomar <gslpalomar02@gmail.com>
2024-10-21 11:49:06 +0200

    Solución ejercicio 3

--- Solucion para estudiar Adventure Works/Ejercicios_lunes_21/Ejercicios.sql
+++ Solucion para estudiar Adventure Works/Ejercicios_lunes_21/Ejercicios.sql
@@ -10,3 +10,9 @@ GROUP BY ca.Nombre;
 select FORMAT(SUM (ProvHab), 'N0') as poblacion_españa from Provincias
 
 --mostrar en dos columnas la población por provincias de Galicia, en orden descendente
+SELECT p.ProvNom, FORMAT(p.ProvHab, 'N0') AS PoblacionGalicia 
+FROM [Provincias] AS p
+inner join [ComunidadesAutonomas] AS ca
+ON ca.ComID = p.ComID
+WHERE ca.ComID = 12
+ORDER BY p.ProvHab DESC;
--- Solucion para estudiar Adventure Works/Proyecto_lunes_21/script_python.py
+++ Solucion para estudiar Adventure Works/Proyecto_lunes_21/script_python.py
@@ -17,7 +17,13 @@ GROUP BY ca.Nombre;'''
 
 query2 = '''select FORMAT(SUM (ProvHab), 'N0') AS poblacion_españa from Provincias'''
 
-query3 = 
+query3 = '''SELECT p.ProvNom, FORMAT(p.ProvHab, 'N0') AS PoblacionGalicia 
+FROM [Provincias] AS p
+inner join [ComunidadesAutonomas] AS ca
+ON ca.ComID = p.ComID
+WHERE ca.ComID = 12
+ORDER BY p.ProvHab DESC;'''
+
 # query4=
 # query5=
 # query6=
@@ -33,9 +39,6 @@ for row in result1:
     values = [
         row[0],                   # Asumimos que la primera columna es un integer
         row[1],                   # Asumimos que la segunda columna es un string
-        # row[2],            # Convertimos la tercera columna a Decimal a float
-        # row[3],            # Convertimos la cuarta  columna Decimal a float
-        # row[4].strftime("%d/%m/%Y")# Formateamos la quinta columna como fecha datetime
     ]
     print(f"{row[0]} tiene {row[1]} provincias")
 
@@ -48,5 +51,15 @@ for row in result2:
     ]
     print(f"España tiene {row[0]} habitantes")
 
-
+cursor.execute(query3)
+result3 = cursor.fetchall()
+    
+print("Lista de las provincias de Galicia y sus respectivos habitantes:  ")
+for row in result3:
+    values = [
+        row[0],
+        row[1]
+    ]
+    print(values)
+    
 conn.close()
